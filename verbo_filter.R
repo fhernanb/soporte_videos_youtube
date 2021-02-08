2d36a2d588debdc136089338701c504ab0065c8f
Ana María <amsanchezh@unal.edu.co>
2021-02-08 16:18:38 -0500

    Se arreglaron los vectores

--- verbo_filter.R
+++ verbo_filter.R
@@ -5,15 +5,22 @@ library(dplyr)            # Para cargar el paquete
 
 # Base de datos de ejemplo
 
-titulo <- c('El Principito', 'Inglés Básico', '')
-autor <- c()
-edicion <- c()
-categoria <- c()
-precio <- c()
-lujo <- c(TRUE, FALSE, FALSE, TRUE, ...)
+titulo <- c('El Principito', 'Inglés Básico', 'Steve Jobs', 
+            'Cuentos Completos', 'La casa de los espiritus', 
+            "Cien años de soledad", "Del amor y otros demonios", 
+            "La Iliada", "Orgullo y prejuicio", "Diccionario escolar")
+autor <- c("De Saint Exupery", "Ghio D.A", "Walter Isaacson",
+           "Allan Poe","Isabel Allende", "Garcia Marquez",
+           "Garcia Marquez","Homero", "Jane Austen", "-")
+editorial <- c("Emece","Editorial Solar", "Debate", "Espuma", 
+               "Debolsillo", "Debolsillo","Debolsillo", "Comcos",
+               "Juventud","Larousse")
+categoria <- c("Literatura Universal", "Idiomas", "Biografias", "Literatura Universal", "Latinoamericana", "Literatura Universal", "Latinoamericana", "Literatura Universal", "Literatura Universal", "Idiomas")
+precio <- c(36000, 28000, 79000, 165000, 41000, 36000, 26000, 23400, 56000, 16000)
+lujo <- c(FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE,FALSE)
 
 dat <- data.frame(titulo=titulo, autor=autor, 
-                  edicion=edicion, categoria=categoria, 
+                  editorial=editorial, categoria=categoria, 
                   precio=precio, lujo=lujo)
 
 # Usando el verbo filter
