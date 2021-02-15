4c8ec8e970927e0fffddc39ad38d998cb4e391cf
Ana María <amsanchezh@unal.edu.co>
2021-02-14 22:08:45 -0500

    Este es el script del verbo group by :smile:

---
+++ verbo_group_by.R
@@ -0,0 +1,46 @@
+# instalar libreria
+install.packages("tidyverse")
+library(tidyverse)
+
+# base de datos
+titulo <- c('Todo ese ayer', 'Una tierra prometida', 
+            'La Presa', 'La casa de los espiritus', 
+            'Tiempos recios', 'Paraíso Travel', 
+            'Cien años de soledad', 'Steve Jobs')
+autor <- c('Oscar Vela', 'Barack Obama',
+           'Irene Nevirovsky','Isabel Allende',
+           'Vargas Llosa', 'Jorge Franco', 'Garcia Marquez',
+           'Walter Isaacson')
+editorial <- c('Alfaguara','Debate', 'Debate', 
+               'Debolsillo','Alfaguara',
+               'Alfaguara', 'Debolsillo', 'Debate')
+categoria <- c('Latinoamericana', 'Biografia', 
+               'Biografia', 'Literatura Universal',
+               'Latinoamericana', 'Latinoamericana', 
+               'Literatura Universal', 'Biografia')
+precio <- c(52000, 79000, 63000, 36000, 54000,  56000, 41000, 79000)
+lujo <- c(TRUE, TRUE, TRUE,  FALSE, TRUE,  TRUE, FALSE,TRUE)
+
+dat <- data.frame(titulo=titulo, autor=autor, 
+                  editorial=editorial, categoria=categoria, 
+                  precio=precio, lujo=lujo)
+
+# conocer el precio máximo por editorial
+dat %>% 
+  group_by(editorial) %>% 
+  summarise(max(precio))
+
+# conocer el precio mínimo por editorial
+dat %>% 
+  group_by(editorial) %>% 
+  summarise(min(precio))
+
+# promedio de precio por categoria
+dat %>% 
+  group_by(categoria) %>% 
+  summarise(mean(precio))
+
+# la función n()
+dat %>% 
+  group_by(categoria) %>% 
+  summarise(conteo = n())
